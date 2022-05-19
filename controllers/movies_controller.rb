4993a0007571fa76e1aa556fb962c9be0a4c951c
MarcusAdams1220 <marcusadams1220@gmail.com>
2022-05-19 11:52:24 +1000

    movie search results and details page update

--- controllers/movies_controller.rb
+++ controllers/movies_controller.rb
@@ -5,31 +5,34 @@ end
 get '/search_results' do
     user_input = params['user_input']
 
-    movies = HTTParty.get("https://omdbapi.com/?s=#{user_input}&apikey=6a610b16")['Search']
+    movies = HTTParty.get("https://omdbapi.com/?s=#{user_input}&apikey=6a610b16")
 
     erb :'movies/search_results', locals: {
-        movies: movies,
+        movies: movies['Search'],
         user_input: user_input
     }
 end
 
 get '/movie_details' do
+    title = params['movie_title']
+
+    details = HTTParty.get("https://omdbapi.com/?t=#{title}&apikey=6a610b16")
 
     #Movie Info
-    movie_title = movie_info['Title']
-    poster_url = movie_info['Poster']
-    realease_date = movie_info['Released']
-    rating = movie_info['Rated']
+    movie_title = details['Title']
+    poster_url = details['Poster']
+    realease_date = details['Released']
+    rating = details['Rated']
 
     # Ratings
-    imdb_rating = movie_info['Ratings'][0]['Value']
-    rotten_tomatoes_rating = movie_info['Ratings'][1]['Value']
-    metacritic_rating = movie_info['Ratings'][2]['Value']
+    imdb_rating = details['Ratings'][0]['Value']
+    rotten_tomatoes_rating = details['Ratings'][1]['Value']
+    metacritic_rating = details['Ratings'][2]['Value']
 
     erb :'movies/movie_details', locals: {
         title: movie_title,
         poster_url: poster_url,
-        realease_date: realease_date,
+        release_date: realease_date,
         rating: rating,
         imdb_rating: imdb_rating,
         rotten_tomatoes_rating: rotten_tomatoes_rating,
